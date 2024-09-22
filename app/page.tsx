e7665caf19e649915fa8d329c00bdec196554a9f
Ritesh Sharma <119002315+RiteshS1@users.noreply.github.com>
2024-09-22 14:07:36 +0530

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -6,7 +6,7 @@ import Hero from "@/components/Hero";
 import Grid from "@/components/Grid";
 import Footer from "@/components/Footer";
 import Clients from "@/components/Clients";
-import Approach from "@/components/Approach";
+// import Approach from "@/components/Approach";
 import Experience from "@/components/Experience";
 import RecentProjects from "@/components/RecentProjects";
 import { FloatingNav } from "@/components/ui/FloatingNavbar";
@@ -21,7 +21,7 @@ const Home = () => {
         <RecentProjects />
         <Clients />
         <Experience />
-        <Approach />
+{/*         <Approach /> */}
         <Footer />
       </div>
     </main>
