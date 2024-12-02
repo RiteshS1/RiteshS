85f905cede6f5a24d6fceaaadfac28fb696a1fa8
Ritesh Sharma <119002315+RiteshS1@users.noreply.github.com>
2024-12-02 18:06:31 +0530

    Update index.ts

--- data/index.ts
+++ data/index.ts
@@ -84,7 +84,7 @@ export const projects = [
     des: "Recreated the Apple iPhone 15 Pro website, combining GSAP animations and Three.js 3D effects..",
     img: "/p4.svg",
     iconLists: ["/next.svg", "/tail.svg", "/ts.svg", "/three.svg", "/gsap.svg"],
-    link: "/ui.apple.com",
+    link: "https://iphone-rs.vercel.app/",
   },
   {
     id: 3,
